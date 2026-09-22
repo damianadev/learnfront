a496a632c1e5251189dd0dae661ee40a1e2484fa
Damian Adamson <Damian.adamson.dev@gmail.com>
2026-09-22 14:13:31 +0300

    Add Vue

--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
-let primaryBtn = document.querySelector('#button');
-console.log({primaryBtn});
-console.log(primaryBtn.classList);
+import { createApp, ref } from 'vue';
 
-primaryBtn.addEventListener('click',() => {
-    if(primaryBtn.classList.contains('is-primary')){ 
-    primaryBtn.classList.replace('is-primary' , 'is-success');
-    } else {
-        primaryBtn.classList.replace('is-success' , 'is-primary');
-    }
-});
+import App from './App.vue';
 
-
-let input = document.querySelector('#input');
-
-let reverseText = document.querySelector('#text');
-
-input.addEventListener('input', () =>{
-    let value = input.value;
-    reverseText.innerText = value.split('').reverse().join('');
-});
+const app = createApp(App);
+app.mount('#app');
