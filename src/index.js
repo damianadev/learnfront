54c4775d4d4e26deb84d7233bc48dff75d594bc1
Damian Adamson <Damian.adamson.dev@gmail.com>
2026-09-22 13:41:44 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
-import math from "./math.js";
+let primaryBtn = document.querySelector('#button');
+console.log({primaryBtn});
+console.log(primaryBtn.classList);
 
-let answer = math.add(2,4);
-console.log(answer);
+primaryBtn.addEventListener('click',() => {
+    if(primaryBtn.classList.contains('is-primary')){ 
+    primaryBtn.classList.replace('is-primary' , 'is-success');
+    } else {
+        primaryBtn.classList.replace('is-success' , 'is-primary');
+    }
+});
+
+
+let input = document.querySelector('#input');
+
+let reverseText = document.querySelector('#text');
+
+input.addEventListener('input', () =>{
+    let value = input.value;
+    reverseText.innerText = value.split('').reverse().join('');
+});
